66e82e8d90ba6e2670939f0f4e495bbe7e839ef0
fyky <fuyu12dmb25ms@gmail.com>
2021-10-14 08:18:26 +0000

    [Add]バリデーションサクセスメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,9 +13,12 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id), notice: 'sucess'
+    else
+    render:new
+    end
   end
 
 
