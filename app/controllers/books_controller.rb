2dd0f7cf4f0d79c768577fedb5fd94cc9165e7fa
fyky <fuyu12dmb25ms@gmail.com>
2021-10-14 06:41:57 +0000

    [Add]Edit機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,9 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
+  
 
   def new
     @book=Book.new
@@ -18,8 +20,15 @@ class BooksController < ApplicationController
 
 
   def edit
+    @book =Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+  
   private
   def book_params
     params.require(:book).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'books' => 'books#index'
   get '' => 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
