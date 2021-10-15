752cb61d512d7c78cba45a15a4ca6fba33d6e778
fyky <fuyu12dmb25ms@gmail.com>
2021-10-15 05:27:54 +0000

    [Update]editupdateメッセージの修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,7 +32,7 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.find(params[:id])
     if @book.update(book_params)
-    redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
     else
     render:edit
     end
