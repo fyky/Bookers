92e612450e7e62b9da3e1a521042272e630d7423
fyky <fuyu12dmb25ms@gmail.com>
2021-10-15 05:16:28 +0000

    [Fix]エラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :books
   
   get 'books' => 'books#index'
-  
-  get '' => 'homes#top'
+  root to: 'homes#top'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
